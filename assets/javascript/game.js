aed120fa5112dc7aa0528dfb97c23f2aa637eaa7
apenachi <apenachi@yahoo.com>
2016-05-26 17:13:52 -0400

    hangman game comparing user input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,14 +6,14 @@ var teams = ['Bears', 'Bengals', 'Bills', 'Broncos', 'Browns', 'Buccaneers', 'Co
 var computerPick = teams[Math.floor(Math.random() * teams.length)];
 // Loops thru each char in computerPick
 for (i = 0; i < computerPick.length; i++) {
-	// Create html <i> tag for each letter in computerPick
+	// Create html <i> tag for each letter in computerPick, and gives it a class and id name
 	var html = '<i class="computerPick" id="'+ i +'" >' + computerPick[i] + '</i>'
 	// Renders html
 	document.querySelector('.phrase').innerHTML = document.querySelector('.phrase').innerHTML + html;
 }
 // Loops thru each item in abc array
 for (i = 0; i < abc.length; i++) {
-	// Create html <i> tag for each item in abc array
+	// Create html <i> tag for each item in abc array and gives it a class and id name
 	var html = '<i class="char" id="'+ abc[i] +'">' + abc[i] + '</i>'
 	// Renders html
 	document.querySelector('.alphabet').innerHTML = document.querySelector('.alphabet').innerHTML + html;
@@ -22,9 +22,11 @@ var tries = 0;
 document.onkeyup = function(event) {
 	// Gets key pressed
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-	// 
+	// Loops thru each item in abc array
 	for (i = 0; i < abc.length; i++) {
+		// Finds item in abc that matches userGuess
 		if (abc[i].toLowerCase() === userGuess) {
+			// Change CSS properties to the item found in abc array
 			document.getElementById(abc[i].toUpperCase()).style.color = "#abc";
 			document.getElementById(abc[i].toUpperCase()).style.textDecoration = "line-through";
 			document.getElementById(abc[i].toUpperCase()).style.fontFamily = "Oxygen";
@@ -38,4 +40,10 @@ document.onkeyup = function(event) {
 			console.log('found : ' + found);
 		}
 	}
+	if (!found) {
+		//tries ++;
+	}
+	// console.log('tries : ' + tries);
+	document.getElementById("hangman").style.backgroundImage = "url('assets/images/hangman." + tries.toString() + ".jpeg')";
+
 }
