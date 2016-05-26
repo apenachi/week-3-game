28cb6450aa5aa05386a7b75153a4eac3929f550e
apenachi <apenachi@yahoo.com>
2016-05-26 17:00:43 -0400

    hangman game rendering html tags

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,41 @@
+// Initialized array with possible values
 var abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M' , 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+// Initialized array with NFL team names
 var teams = ['Bears', 'Bengals', 'Bills', 'Broncos', 'Browns', 'Buccaneers', 'Colts', 'Cardinals', 'Chargers', 'Chiefs', 'Cowboys', 'Dolphins', 'Eagles', 'Falcons', 'Giants', 'Jaguars', 'Jets', 'Lions', 'Packers', 'Panthers', 'Patriots', 'Redskins', 'Raiders', 'Rams', 'Ravens', 'Saints', 'Seahawks', 'Steelers', 'Texans', 'Titans', 'Vikings', 'Fortyniners'];
+// Picks a random team name from teams array
 var computerPick = teams[Math.floor(Math.random() * teams.length)];
-
-// document.querySelector('.phrase').innerHTML = computerPick.length;
-
+// Loops thru each char in computerPick
 for (i = 0; i < computerPick.length; i++) {
-	// var html = '<i class="computerPick">' + computerPick[i] + '</i>'
+	// Create html <i> tag for each letter in computerPick
 	var html = '<i class="computerPick" id="'+ i +'" >' + computerPick[i] + '</i>'
+	// Renders html
 	document.querySelector('.phrase').innerHTML = document.querySelector('.phrase').innerHTML + html;
 }
-
+// Loops thru each item in abc array
 for (i = 0; i < abc.length; i++) {
+	// Create html <i> tag for each item in abc array
 	var html = '<i class="char" id="'+ abc[i] +'">' + abc[i] + '</i>'
+	// Renders html
 	document.querySelector('.alphabet').innerHTML = document.querySelector('.alphabet').innerHTML + html;
 }
 var tries = 0;
 document.onkeyup = function(event) {
+	// Gets key pressed
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
+	// 
 	for (i = 0; i < abc.length; i++) {
 		if (abc[i].toLowerCase() === userGuess) {
 			document.getElementById(abc[i].toUpperCase()).style.color = "#abc";
 			document.getElementById(abc[i].toUpperCase()).style.textDecoration = "line-through";
 			document.getElementById(abc[i].toUpperCase()).style.fontFamily = "Oxygen";
+		}
+	}
+	var found = false;
+	for (var i = 0; i < computerPick.length; i++) {
+		if (computerPick[i].toLowerCase() === userGuess) {
+			document.getElementById(i.toString()).style.color = "#fff";
+			found	= true;
+			console.log('found : ' + found);
 		}
 	}
 }
