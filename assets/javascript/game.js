5e0784980d4c43484e7c0f0c41902fdb2b6884c8
apenachi <apenachi@yahoo.com>
2016-05-28 09:50:30 -0400

    hangman background images added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,8 @@ var computerPick = teams[Math.floor(Math.random() * teams.length)];
 // Loops thru each char in computerPick
 for (i = 0; i < computerPick.length; i++) {
 	// Create html <i> tag for each letter in computerPick, and gives it a class and id name
-	var html = '<i class="computerPick" id="'+ i +'" >' + computerPick[i] + '</i>'
+	// var html = '<i class="computerPick" id="'+ i +'" >' + computerPick[i] + '</i>'
+	var html = '<i class="computerPick" id="'+ i +'" >' + ' ' + '</i>'
 	// Renders html
 	document.querySelector('.phrase').innerHTML = document.querySelector('.phrase').innerHTML + html;
 }
@@ -18,8 +19,10 @@ for (i = 0; i < abc.length; i++) {
 	// Renders html
 	document.querySelector('.alphabet').innerHTML = document.querySelector('.alphabet').innerHTML + html;
 }
-var tries = 0;
+var badGuesses = 0;
+var goodGuesses = 0;
 document.onkeyup = function(event) {
+
 	// Gets key pressed
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 	// Loops thru each item in abc array
@@ -27,7 +30,7 @@ document.onkeyup = function(event) {
 		// Finds item in abc that matches userGuess
 		if (abc[i].toLowerCase() === userGuess) {
 			// Change CSS properties to the item found in abc array
-			document.getElementById(abc[i].toUpperCase()).style.color = "#abc";
+			document.getElementById(abc[i].toUpperCase()).style.color = "#979794";
 			document.getElementById(abc[i].toUpperCase()).style.textDecoration = "line-through";
 			document.getElementById(abc[i].toUpperCase()).style.fontFamily = "Oxygen";
 		}
@@ -35,15 +38,37 @@ document.onkeyup = function(event) {
 	var found = false;
 	for (var i = 0; i < computerPick.length; i++) {
 		if (computerPick[i].toLowerCase() === userGuess) {
+			document.getElementById(i.toString()).innerHTML = userGuess.toUpperCase();
+			document.getElementById(i.toString()).style.backgroundColor = "#33CC33";
 			document.getElementById(i.toString()).style.color = "#fff";
+			document.getElementById(i.toString()).style.backgroundImage = "url('none.jpeg')";
 			found	= true;
+			goodGuesses ++
 			console.log('found : ' + found);
 		}
 	}
+	var play = document.getElementById("keysound");
 	if (!found) {
-		//tries ++;
+		badGuesses ++;
+		play.src = "assets/mp3/Wrong.mp3";
+	} else {
+		play.src = "assets/mp3/Right.mp3";
 	}
-	// console.log('tries : ' + tries);
-	document.getElementById("hangman").style.backgroundImage = "url('assets/images/hangman." + tries.toString() + ".jpeg')";
+	play.load();
+
+	console.log('badGuesses : ' + badGuesses);
+	document.getElementById("hangman").style.backgroundImage = "url('assets/images/hangman." + badGuesses.toString() + ".jpeg')";
+	// You lose
+	if (badGuesses === 6) {
+		play.src = "assets/mp3/ScaryScream.mp3";
+		play.load();
+		document.onkeyup = null;
+		location.reload();
+		// return false;
+	} else if (goodGuesses === computerPick.length) {
+
+	}
 
 }
+
+
